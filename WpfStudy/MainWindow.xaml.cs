9a027f5d35efff4ab664dc36dd184b1c690b88c4
kulbo <kulbo@DESKTOP-K10PK10>
2024-02-03 11:16:52 +0900

    초기 커밋.

---
+++ WpfStudy/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfStudy.Models;
+using WpfStudy.ViewModels;
+
+namespace WpfStudy
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainViewModel mainViewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainViewModel = new MainViewModel();
+            mainViewModel.ProgressValue = 30;
+            DataContext = mainViewModel;
+        }
+
+        private void btnTest1_Click(object sender, RoutedEventArgs e)
+        {
+            //List<User> myList1 = new List<User>();
+            //labelTest1.Content = "내용변경완료";
+            //// MessageBox.Show(textBox1.Text);
+            //User userA = new User();
+            //userA.UserImg = @"D:\tech\src\net\wpf\Park\WpfStudi(2019)\WpfStudy\WpfStudy\Resources\photo1.jpg";
+            //userA.Name = "Noah";
+            //userA.UserAge = 15;
+
+            //User userB = new User();
+            //userB.UserImg = @"D:\tech\src\net\wpf\Park\WpfStudi(2019)\WpfStudy\WpfStudy\Resources\photo2.jpg";
+            //userB.Name = "Liam";
+            //userB.UserAge = 20;
+
+            //myList1.Add(userA);
+            //myList1.Add(userB);
+
+            //listView1.ItemsSource = myList1;
+            //mainViewModel.ProgressValue = 100;
+        }
+    }
+}
---
+++ WpfStudy/ViewModels/MainViewModel.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfStudy.Views;
+
+namespace WpfStudy.ViewModels
+{
+
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private int progressValue;
+        public ICommand TestClick { get; set; }
+
+        public ICommand SecondShow { get; set; }
+        public MainViewModel()
+        {
+            //TestClick = new RelayCommand<object>(ExecuteMyButton, CanMyButton);
+            TestClick = new AsyncRelayCommand(ExecuteMyButton2);
+            SecondShow = new AsyncRelayCommand(ExecuteMyButton3);
+            // Task t = ExecuteMyButton2();
+        }
+        public int ProgressValue
+        {
+            get { return progressValue; }
+            set { 
+                progressValue = value;
+                NotifyPropertyChanged(nameof(ProgressValue));
+            }
+        }
+
+        bool CanMyButton(object param)
+        {
+            if (param == null)
+            {
+                return true;
+            }
+            return param.ToString().Equals("ABC")? true : false;
+        }
+
+        void ExecuteMyButton(object param)
+        {
+            int w = 0;
+            //MessageBox.Show(param.ToString() + "AAA");
+            Task task1 = Task.Run(() => 
+            {
+                for(int i = 0; i < 10; i++)
+                {
+                    ProgressValue = i;
+                }
+            });
+
+            Task rtn2 = Task.Run(() =>
+            {
+                for(int i = 0; i < 50; i++)
+                {
+                    ProgressValue = i;
+                    w = i;
+                    Thread.Sleep(2000);
+                }
+            });
+
+            //rtn2.Wait();
+            MessageBox.Show(w + "");
+        }
+
+        public async Task ExecuteMyButton2()
+        {
+            int w = 0;
+            //MessageBox.Show(param.ToString() + "AAA");
+
+            Task<int> rtn2 = Task.Run(() =>
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    ProgressValue = i;
+                    w = i;
+                    Thread.Sleep(2000);
+                }
+                int j = 5;
+                return j;
+            });
+
+            w = await rtn2;
+
+            MessageBox.Show(w + "");
+        }
+
+        public async Task ExecuteMyButton3()
+        {
+            SecondView secondView = new SecondView();
+            SecondViewModel aa = new SecondViewModel();
+            aa.ProgressValue = 70;
+            secondView.DataContext = aa;
+
+            secondView.ShowDialog();
+
+            await Task.Delay(0);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // This method is called by the Set accessor of each property.  
+        // The CallerMemberName attribute that is applied to the optional propertyName  
+        // parameter causes the property name of the caller to be substituted as an argument.  
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
